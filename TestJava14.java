f2dd2278871e98cfb77e90761efb1a50bf8daf8b
Galih1004 <mochgalihputrapratama@gmail.com>
2025-09-25 12:47:49 +0700

    test

--- TestJava14.java
+++ TestJava14.java
@@ -1,6 +1,6 @@
 public class TestJava14 {
     public static void main(String[] args) {
         System.out.println("Saya Galih membuat baris pertama");
-        System.out.println("Saya Galih membuat baris kedua");
+        System.out.println("Saya Galih Putra membuat baris kedua");
     }
 }
